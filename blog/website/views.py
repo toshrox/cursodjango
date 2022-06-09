27aa1d5a841cfe5e73681f2d5bcfc168811a6eff
Toshrox <erico_toshi45@hotmail.com>
2022-06-09 16:03:58 -0300

    Adicionei comandos de html no template e criei variaveis e lista

--- blog/website/views.py
+++ blog/website/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 
 def tosh(request):
-    return render(request, 'index.html')
+    lista = [
+        'Django', 'Python', 'VSC', 
+        'Git', 'Banco de Dados'
+        ]
+    data = {'erico':'Olá meu nome é Érico', 'mariana': 'E eu sou a Mariana', 'lista_tecnologias': lista}
+    
+    return render(request, 'index.html',data)
 
