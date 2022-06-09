a1dd2bc6868e79e624e7bf4df038c761c301d4c1
Toshrox <erico_toshi45@hotmail.com>
2022-06-09 15:29:29 -0300

    Alterei as urls e views do website

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import hello_world
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hello_world),
+    path('tosh/', include('website.urls')),
+
 ]
--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def tosh(request):
+    return HttpResponse("Hello Toshs2")
+
